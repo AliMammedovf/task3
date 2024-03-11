1bca30811b87f7718c419a1ac9068ce8a1478838
AliMammedovf <tu7dw5n4h@code.edu.az>
2024-03-12 02:21:29 +0400

    uptaded

---
+++ Arrray/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+namespace Arrray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Array'dəki ən kiçik elementi tapıb ekrana yazdırın.
+
+            //int[] arr = { 2, 3, 4, 5, 6 };
+            //int minNumber = arr[0];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < minNumber)
+            //    {
+            //        minNumber = arr[i];
+            //    }
+            //}
+            //Console.WriteLine(minNumber);
+
+            //2.Array'in içərisindəki sözləri tərsinə çevirən bir alqoritm yazın.
+            //string[] arr = { "Ali", "Revane", "Murad", "Gulsum" };
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = arr[i].Length - 1; j >= 0; j--)
+            //    {
+            //        Console.Write(arr[i][j]);
+            //    }
+            //}
+
+
+            //3.Array'in içərisindəki sözlərin sırasını tərsinə çevirən bir alqoritm yazın.
+
+            //string[] arr = { "Ali", "Revane", "Murad", "Gulsum" };
+
+            //for (int i = arr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //4.Verilmiş integer array-in  bütün elementlərini müsbətə çevirib yeni bir array'ə tərsinə yığın və ekrana çap edin. Məsələn, {-2,3,4,-5} verilibsə, {5,4,3,2} almalıyıq cavab olaraq.
+
+            //int[] arr = { -2, 3, 4, -5 };
+            //int loop = 0;
+
+            //for (loop = arr.Length - 1; loop >= 0; loop--)
+            //{
+            //    if (arr[loop] < 0)
+            //    {
+            //        arr[loop] = -arr[loop];
+            //    }
+
+            //    Console.WriteLine(arr[loop]);
+
+            //}
+
+            //5.Verilmiş integer array-in  bütün elementlərini stringə çevirib ekrana çap edin.Məsələn, { -2,3,4,-5}
+            //verilibsə, "-2 3 4 -5" çapa verilməlidir. 
+
+            //int[] arr = { -2, 3, 4, -5 };
+            //string[] strarr = { "-2", "3", "4", "-5" };
+
+            //for(int i = 0; i < arr.Length; i++)
+            //{
+            //    strarr[i] = arr[i] + "";
+            //}
+            //Console.WriteLine(string.Join(" ",strarr));  
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
